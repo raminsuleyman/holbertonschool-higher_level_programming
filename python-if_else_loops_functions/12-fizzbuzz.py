fdbef1dcf49d83d9b2aa1846870b757f43ae676c
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-04-11 14:47:42 +0000

    11

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("{}".format("FizzBuzz"), end=" ")
+        elif n % 3 == 0:
+            print("{}".format("Fizz"), end=" ")
+        elif n % 5 == 0:
+            print("{}".format("Buzz"), end=" ")
+        else:
+            print(n, end=" ")
+    return

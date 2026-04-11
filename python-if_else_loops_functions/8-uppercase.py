bb40a8c6214956d11120ad21aedeced213238654
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-04-11 14:15:06 +0000

    8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord("a") <= ord(i) <= ord("z"):
+            c = chr(ord(i)-32)
+        print({}.format(i),end="")
+    print()

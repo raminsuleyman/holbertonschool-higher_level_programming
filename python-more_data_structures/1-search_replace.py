7fb6cb6adada07a62ac8380779952b4a12006079
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-04-14 18:29:33 +0000

    0

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
 
     for i in my_list:
         if i == search:
-            new_list.append(replaace)
+            new_list.append(replace)
         else:
             new_list.append(i)
     return new_list

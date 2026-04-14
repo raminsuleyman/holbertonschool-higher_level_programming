170ff5b617a00038aa6022ac27e5adc6d589eca1
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-04-14 18:27:15 +0000

    0

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+
+    for i in my_list:
+        if i == seacrh:
+            new_list.append(replaace)
+        else:
+            new_list.append(i)
+    return new_list

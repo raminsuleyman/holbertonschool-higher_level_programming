93f8c0f9d64bcbfc7ad35b8be7332b141e3d09b6
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-04-14 18:57:49 +0000

    0

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    result = []
+
+    for x in set_1:
+        if x not in set_2:
+            result.append(x)
+
+    for x in set_2:
+        if x not in set_1:
+            result.append(x)
+
+    return set(result)

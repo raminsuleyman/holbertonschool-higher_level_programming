4d34f7565d3e2a7fe3e2a8529442aeae490a20d8
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-04-14 18:54:02 +0000

    0

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pthon3
+def common_elements(set_1, set_2):
+    common_elements = []
+    for i in set_1:
+        if i in set_2:
+            common_elements.append(i)
+    return common_elements

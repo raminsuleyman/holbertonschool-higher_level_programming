890e7ab6af95272f195ad52981747b9b63eed724
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-04-18 19:12:14 +0000

    m

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,17 @@
+#!/usr/bin/python3
+"""Module that adds two integers."""
+
+
 def add_integer(a, b=98):
+    """Add two integers.
+
+    a and b must be integers or floats.
+    If float, they are cast to int.
+    """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-
-    # NaN check (ƏVVƏL!)
-    if a != a:
-        raise TypeError("a must be an integer")
-    if b != b:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)

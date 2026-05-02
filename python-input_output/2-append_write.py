e2a165ada8b5fa0a177fed45b4db189570e46e4e
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-05-02 19:20:44 +0000

    0

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Bu modul faylın sonuna mətn əlavə edən funksiyanı ehtiva edir."""
+
+
+def append_write(filename="", text=""):
+    """
+    Mətni UTF8 faylın sonuna əlavə .
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)

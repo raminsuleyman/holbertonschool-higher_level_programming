acfbca7d970152d2f9851164ab1b4485e902cd3d
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-05-02 19:01:23 +0000

    0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""bu modul atribut ve metidlarin axtarmaq ucun lookup funksiyasi"""
+
+
+def lookup(obj):
+    """obyektin atribut ve metodlarini qaytarir"""
+    return dir(obj)

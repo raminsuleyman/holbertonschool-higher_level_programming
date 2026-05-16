4410d8966e4dece947ba7f7e2f01b8ca7b5921e0
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-05-16 19:42:01 +0000

    9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""prints the first State object"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        )
+    )
+    Session = sessionmaker(bind=engine)
+    d = Session()
+
+    s = d.query(State).filter(State.name.like("%a%")).order_by(State.id).all()
+    for state in s:
+        print("{}: {}".format(state.id, state.name))
+    session.close()

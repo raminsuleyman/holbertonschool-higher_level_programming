af00b342ac6803940b52f05ec680735b9bd7daea
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-06-06 12:27:40 +0000

    0

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const say  = process.argv.length;
 if (say === 2) {
-    console.log('No argument');
-} else if (say === 3) {
-    console.log('Argument found');
+  console.log('No argument');
+} else if (say === 3) 
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }

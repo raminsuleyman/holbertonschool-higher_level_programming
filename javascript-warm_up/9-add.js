d9cc782423a9a106b9c7ae81529e5da2acefb885
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-06-06 19:56:45 +0000

    0

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  return a + b
 }
 
-const first = parseInt(process.argv[2]);
-const second = parseInt(process.argv[3]);
+const first = parseInt(process.argv[2])
+const second = parseInt(process.argv[3])
 
-console.log(add(first, second));
+console.log(add(first, second))

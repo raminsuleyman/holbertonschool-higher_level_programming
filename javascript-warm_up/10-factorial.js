db22fa94239f5f6031de2fa2b2f0a468d21c3b85
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-06-06 19:59:35 +0000

    0

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,10 +2,10 @@
 
 function factorial (n) {
   if (isNaN(n) || n <= 1) {
-    return 1
+    return 1;
   }
 
-  return n * factorial(n - 1)
+  return n * factorial(n - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])))
+console.log(factorial(parseInt(process.argv[2])));

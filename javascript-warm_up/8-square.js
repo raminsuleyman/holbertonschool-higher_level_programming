b0d68a5152270bb395fcf0f17fb087e5586f2261
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-06-06 19:53:05 +0000

    0

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+const first = parseInt(process.argv[2]);
+
+if (isNaN(first)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < first; i++) {
+    let row = '';
+
+    for (let j = 0; j < first; j++) {
+      row += 'X';
+    }
+
+    console.log(row);
+  }
+}

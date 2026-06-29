dcadf2f563cb9bc3c6d28d53beca43b24b412fae
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-06-29 19:29:50 +0000

    11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  const nums = args.map(Number).sort((a, b) => b - a);
+  console.log(nums[1]);
+}
